c590871c9ce402843109ace674cf73c62a60d5e2
Augusto Sampaio <augusto_lbs@hotmail.com>
2024-10-31 20:01:50 -0300

    Investimento.jsp
    Page Investimento em JSP

---
+++ src/main/webapp/resources/js/perfil.js
@@ -0,0 +1,13 @@
+
+    document.getElementById('delete-account').addEventListener('click', function() {
+        if (confirm("Tem certeza de que deseja excluir a conta? Esta ação não pode ser desfeita.")) {
+            // Lógica para excluir a conta
+            alert("Conta excluída com sucesso.");
+        }
+    });
+
+
+    
+
+    
+
